726557ef78872c0007170d79110f2cbcfdb30a98
Aleksandr <aleks.spitsin@gmail.com>
2023-04-07 22:50:49 +0300

    Fix

--- app/bot.js
+++ app/bot.js
@@ -1,4 +1,7 @@
 const TelegramApi = require('node-telegram-bot-api')
 require('dotenv').config()
 
-module.exports = new TelegramApi(process.env.TOKEN, { polling: true })
+module.exports = new TelegramApi(
+  '6117695486:AAGvtuxNdu0hVLg_j6vZal4zJZ1-3qtu3Tw',
+  { polling: true }
+)
