fba123db2624b55f5d364ed8f4f0085732394b63
Aleksandr <aleks.spitsin@gmail.com>
2023-04-08 11:46:36 +0300

    Process web app data

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-const express = require('express')
-const cors = require('cors')
+// const express = require('express')
+// const cors = require('cors')
 
 const bot = require('./app/bot')
 const MESSAGES = require('./app/messages')
 
 const webAppUrl = 'https://operkot.github.io/bestshave-bot-frontend/#/'
 // const webAppUrl = 'https://fa82-81-163-156-8.eu.ngrok.io'
-const PORT = '8080'
+// const PORT = '8080'
 
-const app = express()
+// const app = express()
 
-app.use(express.json())
-app.use(cors())
+// app.use(express.json())
+// app.use(cors())
 
 const start = () => {
   bot.on('message', async msg => {
@@ -48,36 +48,44 @@ const start = () => {
       })
     }
 
-    // return bot.sendMessage(chatId, MESSAGES.UNKNOWN_COMMAND_MSG)
+    if (msg?.web_app_data?.data) {
+      try {
+        const data = JSON.parse(msg?.web_app_data?.data)
+
+        bot.sendMessage(chatId, MESSAGES.ORDER_MSG(data.items, data.total))
+      } catch (error) {}
+    }
+
+    return bot.sendMessage(chatId, MESSAGES.UNKNOWN_COMMAND_MSG)
   })
 }
 
 start()
 
-app.post('/webdata', async (req, res) => {
-  const { queryId, items, total } = req.body
-  try {
-    await bot.answerWebAppQuery(queryId, {
-      type: 'article',
-      id: queryId,
-      title: 'Заказ офрмлен!',
-      input_message_content: {
-        message_text: MESSAGES.ORDER_MSG(items, total),
-      },
-    })
-    return res.status(200).json({})
-  } catch (error) {
-    await bot.answerWebAppQuery(queryId, {
-      type: 'article',
-      id: queryId,
-      title: 'Не удалось оформить заказ!',
-      input_message_content: {
-        message_text: 'Не удалось оформить заказ!',
-        parse_mode: 'Markdown',
-      },
-    })
-    return res.status(500).json({})
-  }
-})
+// app.post('/webdata', async (req, res) => {
+//   const { queryId, items, total } = req.body
+//   try {
+//     await bot.answerWebAppQuery(queryId, {
+//       type: 'article',
+//       id: queryId,
+//       title: 'Заказ офрмлен!',
+//       input_message_content: {
+//         message_text: MESSAGES.ORDER_MSG(items, total),
+//       },
+//     })
+//     return res.status(200).json({})
+//   } catch (error) {
+//     await bot.answerWebAppQuery(queryId, {
+//       type: 'article',
+//       id: queryId,
+//       title: 'Не удалось оформить заказ!',
+//       input_message_content: {
+//         message_text: 'Не удалось оформить заказ!',
+//         parse_mode: 'Markdown',
+//       },
+//     })
+//     return res.status(500).json({})
+//   }
+// })
 
-app.listen(PORT, () => console.log('Server started on port ' + PORT))
+// app.listen(PORT, () => console.log('Server started on port ' + PORT))
